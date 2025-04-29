98f0c65f984e0b3435689621675b783d76ccd45c
joaquin-arzuaga <joaquin.arzuaga@correo.ucu.edu.uy>
2025-04-28 23:34:08 -0300

    fix3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Order from "../Order/Order";
+import { FoodList } from "../FoodList/FoodList";
 
-function App() {
+function App({foodElements}) {
   return (
     <div className="app-container">
       <h1>FoodApp</h1>
       <Order />
+      <FoodList foodElements={foodElements} />
     </div>
   );
 }

1c1c27a0b52153d59f2a82976707821c9dc0cdff
Javier Moreno <javier.moreno@correo.ucu.edu.uy>
2025-04-30 20:29:21 -0300

    json server

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,32 @@
 import Order from "../Order/Order";
 import { FoodList } from "../FoodList/FoodList";
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App({foodElements, orders}) {
+function App() {
+
+  const [foods, setFoods] = useState([]);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/foods')
+      .then(res => res.json())
+      .then(data => setFoods(data));
+  }, []);
+
+
+  useEffect(() => {
+    fetch('http://localhost:3001/orders')
+      .then(res => res.json())
+      .then(data => setOrders(data));
+  }, []);
+
+
   return (
     <div className="app-container">
       <h1>FoodApp</h1>
       <Order orders={orders} />
-      <FoodList foodElements={foodElements} />
+      <FoodList foodElements={foods} />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,9 @@ import { createRoot } from 'react-dom/client'
 
 import App from './components/App/App.jsx'
 
-const PRODUCTS = [
-  {id: "1", price: "$1", stock: 5, img: "src/assets/img/hamburger.png"},
-  {id: "2", price: "$1", stock: 4, img: "src/assets/img/hamburger.png"},
-  {id: "3", price: "$1", stock: 0, img: "src/assets/img/hamburger.png"},
-  {id: "4", price: "$1", stock: 2, img: "src/assets/img/hamburger.png"},
-  {id: "5", price: "$1", stock: 0, img: "src/assets/img/hamburger.png"},
-];
-
-const ORDERS = [
-  {name: "name1", price: 50, quantity: 3},
-  {name: "name2", price: 40, quantity: 2},
-  {name: "name3", price: 50, quantity: 4},
-  {name: "name4", price: 50, quantity: 2},
-  {name: "name5", price: 30, quantity: 2}
-]
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App foodElements={PRODUCTS} orders={ORDERS}/>
+    <App />
   </StrictMode>,
 )

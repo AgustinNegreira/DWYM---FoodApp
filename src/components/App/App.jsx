699215fa6b27384aafb7329a6f16dd30b0c0a1ff
Javier Moreno <javier.moreno@correo.ucu.edu.uy>
2025-04-30 19:51:41 -0300

    a

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,12 @@ import Order from "../Order/Order";
 import { FoodList } from "../FoodList/FoodList";
 
 function App({foodElements}) {
+
+  const addToCart = (food) => {
+    foodElements.foods.find(f => f.name === food.name);
+    
+  } 
+
   return (
     <div className="app-container">
       <h1>FoodApp</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,20 @@ import { createRoot } from 'react-dom/client'
 
 import App from './components/App/App.jsx'
 
-const PRODUCTS = [
-  {id: "1", price: "$1", stock: 5, img: "img"},
-  {id: "2", price: "$1", stock: 4, img: "img2"},
-  {id: "3", price: "$1", stock: 0, img: "img3"},
-  {id: "4", price: "$1", stock: 2, img: "img4"},
-  {id: "5", price: "$1", stock: 0, img: "img5"},
-];
+const PRODUCTS = {
+  foods: [
+    { id: "1", name: "pizza", price: "$1", stock: 5, img: "img" },
+    { id: "2", name: "burger", price: "$1", stock: 4, img: "img2" },
+    { id: "3", name: "hotdog", price: "$1", stock: 0, img: "img3" },
+    { id: "4", name: "shake", price: "$1", stock: 2, img: "img4" },
+    { id: "5", name: "burrito", price: "$1", stock: 0, img: "img5" }
+  ],
+  orders: []
+};
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App foodElements={PRODUCTS}/>
+    <App foodElements={PRODUCTS} />
   </StrictMode>,
 )
